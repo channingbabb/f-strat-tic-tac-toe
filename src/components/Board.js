cbaccff7168d813bdc7e1014eb39a2fe46f4aee8
channingbabb <channingbabb@gmail.com>
2023-11-14 19:30:09 -0600

    fixes

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,142 +1,196 @@
 import React, { Component } from "react";
-import Square from "./Cell";
 import "../assets/Board.css";
-
-const AI_ENABLED = true;
+import Square from "./Cell";
 
 class Board extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            squares: Array(9).fill(null),
-            xIsNext: true,
-            done: false,
-            thinking: false,
-        };
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      squares: Array(9).fill(null),
+      xIsNext: true,
+      done: false,
+      thinking: false,
+    };
+  }
 
-    handleClick(i) {
-        if (!this.state.thinking) {
-            const squares = this.state.squares.slice();
-            if (calculateWinner(squares) || squares[i]) {
-                return;
-            }
-            squares[i] = this.state.xIsNext ? "X" : "O";
+  componentDidMount() {
+    if (this.props.aiChooses) {
+        console.log("HERE")
+        const squares = this.state.squares.slice();
+        this.setState({ thinking: true });
+        setTimeout(() => {
+            // AI makes a random move for O
+            const emptySquares = squares.reduce((acc, val, idx) => {
+                if (!val) acc.push(idx);
+                return acc;
+            }, []);
+            const randomIndex = Math.floor(Math.random() * emptySquares.length);
+            const randomSquare = emptySquares[randomIndex];
+            squares[randomSquare] = "O";
             this.setState({
                 squares: squares,
                 xIsNext: !this.state.xIsNext,
             });
-
-            const winner = calculateWinner(squares);
-            if (AI_ENABLED && !winner) {
-                this.setState({ thinking: true });
-                setTimeout(() => {
-                    // AI makes a random move for O
-                    const emptySquares = squares.reduce((acc, val, idx) => {
-                        if (!val) acc.push(idx);
-                        return acc;
-                    }, []);
-                    const randomIndex = Math.floor(Math.random() * emptySquares.length);
-                    const randomSquare = emptySquares[randomIndex];
-                    squares[randomSquare] = "O";
-                    this.setState({
-                        squares: squares,
-                        xIsNext: !this.state.xIsNext,
-                    });
-                    const newWinner = calculateWinner(squares);
-                    if (newWinner) {
-                        this.props.onClick(newWinner);
-                    } else if (squares.every((square) => square !== null)) {
-                        this.props.onClick("CAT");
-                    }
-                    this.setState({ thinking: false });
-                }, 500);
+            const newWinner = calculateWinner(squares);
+            if (newWinner) {
+                this.props.onClick(newWinner, randomIndex);
+            } else if (squares.every((square) => square !== null)) {
+                this.props.onClick("CAT", randomIndex);
             } else {
-                if (winner) {
-                    this.props.onClick(winner);
-                } else if (squares.every((square) => square !== null)) {
-                    this.props.onClick("CAT");
+                this.props.onClick(null, randomIndex);
+            }
+            this.setState({ thinking: false });
+        }, 500);
+    }
+  }
+
+  componentDidUpdate(prevProps) {
+    console.log(prevProps);
+    console.log(this.props);
+    if (this.props.aiChooses && (prevProps.aiChooses !== this.props.aiChooses)) {
+        console.log("HERE")
+        const squares = this.state.squares.slice();
+        this.setState({ thinking: true });
+        setTimeout(() => {
+            // AI makes a random move for O
+            var emptySquares = [];
+            for (var i = 0; i < squares.length; i++) {
+                if (!squares[i]) {
+                    emptySquares.push(i);
                 }
+            }
+            // choose random square from empty squares
+            const randomIndex = Math.floor(Math.random() * emptySquares.length);
+            const randomSquare = emptySquares[randomIndex];
+            squares[randomSquare] = "O";
+            this.setState({
+                squares: squares,
+                xIsNext: !this.state.xIsNext,
+            });
+            const newWinner = calculateWinner(squares);
+            if (newWinner) {
+                this.props.onClick(newWinner, randomIndex);
+            } else if (squares.every((square) => square !== null)) {
+                this.props.onClick("CAT", randomIndex);
+            } else {
+                this.props.onClick(null, randomIndex);
+            }
+            this.props.setChosenBoard(randomIndex);
+            console.log("set chosen board " + randomIndex);
+            this.setState({ thinking: false });
+        }, 500);
+    }
+  }
+
+  handleClick(i) {
+    if (!this.state.thinking && this.props.canChoose && !this.props.aiChooses) {
+        const squares = this.state.squares.slice();
+        if (calculateWinner(squares) || squares[i]) {
+            return;
+        }
+        squares[i] = this.state.xIsNext ? "X" : "O";
+        this.setState({
+            squares: squares,
+            xIsNext: !this.state.xIsNext,
+        });
+
+        const winner = calculateWinner(squares);
+        if (this.props.aiChooses) {
+
+        } else {
+            if (winner) {
+                this.props.onClick(winner, i);
+            } else if (squares.every((square) => square !== null)) {
+                this.props.onClick("CAT", i);
+            } else {
+                this.props.onClick(null, i);
             }
         }
     }
+}
 
-    renderSquare(i) {
-        return (
-            <Square
-                value={this.state.squares[i]}
-                onClick={() => this.handleClick(i)}
-            />
-        );
+  renderSquare(i) {
+    return (
+      <Square
+        value={this.state.squares[i]}
+        onClick={() => this.handleClick(i)}
+      />
+    );
+  }
+
+  render() {
+    var done = false;
+    const winner = calculateWinner(this.state.squares);
+    let status;
+    if (winner) {
+      status = "Winner: " + winner;
+      done = true;
+    } else if (this.state.squares.every((square) => square !== null)) {
+      status = "It's a tie!";
+      done = true;
     }
 
-    render() {
-        var done = false;
-        const winner = calculateWinner(this.state.squares);
-        let status;
-        if (winner) {
-            status = "Winner: " + winner;
-            done = true;
-        } else if (this.state.squares.every((square) => square !== null)) {
-            status = "It's a tie!";
-            done = true;
-        }
+    var rowContainerClass = done ? "rowContainer disabled" : "rowContainer";
+    var statusClass =
+      done && winner === "X"
+        ? "status x"
+        : done && winner === "O"
+        ? "status o"
+        : "status";
 
-        var rowContainerClass = done ? "rowContainer disabled" : "rowContainer";
-        var statusClass =
-            done && winner === "X"
-                ? "status x"
-                : done && winner === "O"
-                ? "status o"
-                : "status";
-
-        return (
-            <div>
-                <div className={statusClass}>{status}</div>
-                <div className="ai-status o">{AI_ENABLED && done && winner === "X" ? "AI sucks" : AI_ENABLED && done && winner === "O" ? "You suck" : ""}</div>
-                <div className="ai-status">
-                    {this.state.thinking ? "AI is thinking..." : ""}
-                </div>
-                <div className={rowContainerClass}>
-                    <div className="board-row">
-                        {this.renderSquare(0)}
-                        {this.renderSquare(1)}
-                        {this.renderSquare(2)}
-                    </div>
-                    <div className="board-row">
-                        {this.renderSquare(3)}
-                        {this.renderSquare(4)}
-                        {this.renderSquare(5)}
-                    </div>
-                    <div className="board-row">
-                        {this.renderSquare(6)}
-                        {this.renderSquare(7)}
-                        {this.renderSquare(8)}
-                    </div>
-                </div>
-            </div>
-        );
-    }
+    return (
+      <div className={"boardContainer"}>
+        <div className={statusClass}>{status}</div>
+        <div className="ai-status o">
+          {this.props.AI_ENABLED && done && winner === "X"
+            ? "AI sucks"
+            : this.props.AI_ENABLED && done && winner === "O"
+            ? "You suck"
+            : ""}
+        </div>
+        <div className="ai-status">
+          {this.state.thinking ? "AI is thinking..." : ""}
+        </div>
+        <div className={rowContainerClass}>
+          <div className="board-row">
+            {this.renderSquare(0)}
+            {this.renderSquare(1)}
+            {this.renderSquare(2)}
+          </div>
+          <div className="board-row">
+            {this.renderSquare(3)}
+            {this.renderSquare(4)}
+            {this.renderSquare(5)}
+          </div>
+          <div className="board-row">
+            {this.renderSquare(6)}
+            {this.renderSquare(7)}
+            {this.renderSquare(8)}
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Board;
 
 function calculateWinner(squares) {
-    const lines = [
-        [0, 1, 2],
-        [3, 4, 5],
-        [6, 7, 8],
-        [0, 3, 6],
-        [1, 4, 7],
-        [2, 5, 8],
-        [0, 4, 8],
-        [2, 4, 6],
-    ];
-    for (let i = 0; i < lines.length; i++) {
-        const [a, b, c] = lines[i];
-        if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-            return squares[a];
-        }
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
     }
-    return null;
+  }
+  return null;
 }
--- src/components/Game.js
+++ src/components/Game.js
@@ -1,20 +1,33 @@
 import React, { useState } from "react";
-import Board from "./Board";
 import "../assets/Game.css";
+import Board from "./Board";
+
+// function RenderBoard(boardIndex, chosenBoard) {
+//   return (
+//     <Board
+//       squares={boards[boardIndex]}
+//       onClick={(winner) => handleClick(boardIndex, winner)}
+//       canChoose={(chosenBoard === null || chosenBoard === boardIndex) && !calculateWinner(boards[boardIndex])}
+//     />
+//   );
+// }
+
+const AI_ENABLED = true;
 
   function Game() {
     const [boards, setBoards] = useState(Array(9).fill(Array(9).fill(null)));
     const [xIsNext, setXIsNext] = useState(true);
     const [winCount, setWinCount] = useState({ X: 0, O: 0, CAT: 0 }); // new state to keep track of win count
+    const [chosenBoard, setChosenBoard] = useState(null); // new state to keep track of chosen board
 
 
-    function handleClick(boardIndex, winner) {
+    function handleClick(boardIndex, winner, i) {
       const newBoards = [...boards];
       const squares = [...newBoards[boardIndex]];
       newBoards[boardIndex] = squares;
-    
       setBoards(newBoards);
       setXIsNext(!xIsNext);
+      setChosenBoard(i);
     
       // update win count if there is a winner
       if (winner) {
@@ -49,14 +62,7 @@ import "../assets/Game.css";
       return null;
     }
 
-    function renderBoard(boardIndex) {
-      return (
-        <Board
-          squares={boards[boardIndex]}
-          onClick={(winner) => handleClick(boardIndex, winner)}
-        />
-      );
-    }
+
 
     function renderStatus() {
       const winner = calculateWinner(boards.flat());
@@ -80,21 +86,19 @@ import "../assets/Game.css";
           </div>
         </div>
         <div className="game-boards">
-          <div className="board-row">
-            {renderBoard(0)}
-            {renderBoard(1)}
-            {renderBoard(2)}
-          </div>
-          <div className="board-row">
-            {renderBoard(3)}
-            {renderBoard(4)}
-            {renderBoard(5)}
-          </div>
-          <div className="board-row">
-            {renderBoard(6)}
-            {renderBoard(7)}
-            {renderBoard(8)}
-          </div>
+            {/** create new map */
+              boards.map((board, i) => (
+                <Board
+                  key={i}
+                  squares={board}
+                  onClick={(winner, cell) => handleClick(i, winner, cell)}
+                  canChoose={(chosenBoard === null || chosenBoard === i) && !calculateWinner(board)}
+                  AI_ENABLED={AI_ENABLED}
+                  aiChooses={AI_ENABLED && !xIsNext && (chosenBoard === null || chosenBoard === i)}
+                  setChosenBoard={setChosenBoard}
+                />
+              ))
+            }
         </div>
       </div>
     );
